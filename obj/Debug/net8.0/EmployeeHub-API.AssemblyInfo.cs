5cbfc77ce9cf504eebd5a8467dc67f92d31a36e1
khaledd20 <khaledmohameda70@gmail.com>
2025-01-07 18:23:18 +0200

    sprint 1

--- obj/Debug/net8.0/EmployeeHub-API.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmployeeHub-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeHub-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff147cb4c2522b3944e5015f9f7e96fcab233c4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1730b2cff592651d7fa10a623a6179384913ccd")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeHub-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeHub-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
